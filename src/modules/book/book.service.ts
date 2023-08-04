784bd5728c5d2e017be79becde957d282999d6aa
Michael Pereira <michael.pereira@chat2desk.com.br>
2023-08-04 17:12:36 -0300

    feat: add book

---
+++ src/modules/book/book.service.ts
@@ -0,0 +1,78 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { PrismaService } from 'src/shared/database';
+
+@Injectable()
+export class BookService {
+  constructor(private readonly prismaService: PrismaService) {}
+  async create(createBookDto: CreateBookDto) {
+    const book = await this.prismaService.book.findMany({
+      where: { title: createBookDto.title },
+    });
+    console.log(book.length);
+    if (book.length) {
+      throw new BadRequestException(
+        `Book ${createBookDto.title} already exists`,
+      );
+    }
+    return await this.prismaService.book.create({ data: createBookDto });
+  }
+
+  async findAll() {
+    return await this.prismaService.book.findMany({
+      include: {
+        author: true,
+      },
+    });
+  }
+
+  async findOne(id: string) {
+    const book = await this.prismaService.book.findMany({
+      where: { id },
+      include: {
+        author: true,
+      },
+    });
+
+    if (!book) {
+      throw new NotFoundException('Book not found');
+    }
+
+    return book;
+  }
+
+  async update(id: string, updateBookDto: UpdateBookDto) {
+    const book = await this.prismaService.book.findMany({
+      where: { id },
+    });
+
+    if (!book) {
+      throw new NotFoundException('Book not found');
+    }
+
+    return await this.prismaService.book.update({
+      where: { id },
+      include: {
+        author: true,
+      },
+      data: updateBookDto,
+    });
+  }
+
+  async delete(id: string) {
+    const book = await this.prismaService.book.findMany({
+      where: { id },
+    });
+
+    if (!book) {
+      throw new NotFoundException('Book not found');
+    }
+
+    await this.prismaService.book.delete({ where: { id } });
+  }
+}
---
+++ src/modules/book/dto/create-book.dto.ts
@@ -0,0 +1,8 @@
+export class CreateBookDto {
+  title: string;
+  gender: string;
+  language: string;
+  published: string;
+  synopsis: string;
+  authorId: string;
+}
