16d8f05abf75f0374b83de61ae9d60d20b3a57d6
Michael Pereira <michael.pereira@chat2desk.com.br>
2023-08-04 17:08:38 -0300

    feat: add database

---
+++ prisma/migrations/20230804200801_create_database/migration.sql
@@ -0,0 +1,68 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('SAVE', 'READ');
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "author" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "birth" TEXT NOT NULL,
+    "nationality" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "book" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+    "language" TEXT NOT NULL,
+    "published" TEXT NOT NULL,
+    "synopsis" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "authorId" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "favorite" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "bookId" TEXT NOT NULL,
+    "status" "Status" NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_id_key" ON "user"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "author_id_key" ON "author"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "book_id_key" ON "book"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "favorite_id_key" ON "favorite"("id");
+
+-- AddForeignKey
+ALTER TABLE "book" ADD CONSTRAINT "book_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "author"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "favorite" ADD CONSTRAINT "favorite_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "favorite" ADD CONSTRAINT "favorite_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "book"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
