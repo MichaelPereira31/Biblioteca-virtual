5f6fb5c6a2809be149b1a4020d444d51c39a7cd7
Michael Pereira <michael.pereira@chat2desk.com.br>
2023-08-06 13:18:40 -0300

    feat: add validation on author routes

--- src/modules/author/author.service.ts
+++ src/modules/author/author.service.ts
@@ -1,8 +1,4 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAuthorDto } from './dto/create-author.dto';
 import { UpdateAuthorDto } from './dto/update-author.dto';
 import { PrismaService } from 'src/shared/database';
