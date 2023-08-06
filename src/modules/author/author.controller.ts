1e4d5e255cc0f51cb27ac9ec641f1f240c96f6d8
Michael Pereira <michael.pereira@chat2desk.com.br>
2023-08-06 13:18:28 -0300

    feat: add validation on author routes

--- src/modules/author/author.controller.ts
+++ src/modules/author/author.controller.ts
@@ -6,36 +6,43 @@ import {
   Param,
   Delete,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { AuthorService } from './author.service';
 import { CreateAuthorDto } from './dto/create-author.dto';
 import { UpdateAuthorDto } from './dto/update-author.dto';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('author')
 export class AuthorController {
   constructor(private readonly authorService: AuthorService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   create(@Body() createAuthorDto: CreateAuthorDto) {
     return this.authorService.create(createAuthorDto);
   }
 
   @Get()
+  @UseGuards(AuthGuard)
   findAll() {
     return this.authorService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard)
   findOne(@Param('id') id: string) {
     return this.authorService.findOne(id);
   }
 
   @Put(':id')
+  @UseGuards(AuthGuard)
   update(@Param('id') id: string, @Body() updateAuthorDto: UpdateAuthorDto) {
     return this.authorService.update(id, updateAuthorDto);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard)
   remove(@Param('id') id: string) {
     return this.authorService.delete(id);
   }
